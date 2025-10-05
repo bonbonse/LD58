ec2e3a5bb9aeb83f22102f8be83b629320d316f1
Bondar <cemenbond@mail.ru>
2025-10-05 19:03:56 +0300

    anim start

---
+++ Assets/_Project/Scripts/Animations/AnimationPlayer.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Ludum.Animations 
+{
+    public class AnimationPlayer : MonoBehaviour
+    {
+        public float DeltaTime = 0;
+        public bool Loop = true;
+
+        protected Material targetMaterial;
+        public Texture2D[] sprites;
+
+        public MeshRenderer meshRenderer;
+
+        private int currentIndex = 0;
+
+        void Start()
+        {
+            if (meshRenderer == null)
+            {
+                meshRenderer = GetComponent<MeshRenderer>();
+            }
+            // ≈сли материал не назначен в инспекторе, берем из рендерера
+            if (targetMaterial == null)
+            {
+                targetMaterial = meshRenderer.materials[0];
+            }
+            StartAnimation();
+        }
+
+
+        public void StartAnimation()
+        {
+            currentIndex = 0;
+            StartCoroutine(WaitAndNextSprite());
+        }
+
+        // ћетод дл€ смены по индексу из массива
+        public void ChangeSpriteByIndex(int index)
+        {
+            if (sprites != null && index >= 0 && index < sprites.Length)
+            {
+                targetMaterial.mainTexture = sprites[index];
+                StartCoroutine(WaitAndNextSprite());
+            }
+        }
+
+        protected IEnumerator WaitAndNextSprite()
+        {
+            yield return new WaitForSeconds(DeltaTime);
+            if (currentIndex >= sprites.Length)
+            {
+                if (Loop)
+                {
+                    currentIndex = 0;
+                }
+                else
+                {
+                    ChangeSpriteByIndex(0);
+                    yield return null;
+                }
+            }
+            ChangeSpriteByIndex(currentIndex);
+            currentIndex++;
+        }
+    }
+}
+
